4ac8681674deca835931fb4163c8f17a1bf3b23d
Cyber-fungus <internationalspy17@gmail.com>
2026-08-21 10:36:25 +0530

    feat: add token and positional embedding

---
+++ src/embedding.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+
+class GPTEmbedding(nn.Module):
+    def __init__(self,vocab_size,context_length,embedding_dim):
+        super().__init__()
+
+        self.token_embedding = nn.Embedding(
+            vocab_size,
+            embedding_dim
+        )
+
+        self.position_embedding = nn.Embedding(
+            context_length,
+            embedding_dim
+        )
+
+    def forward(self, token_ids):
+
+        positions = torch.arange(
+            token_ids.shape[1],
+            device=token_ids.device
+        )
+
+        token_emb = self.token_embedding(token_ids)
+        position_emb = self.position_embedding(positions)
+
+        return token_emb + position_emb
