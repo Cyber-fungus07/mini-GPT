92fff4ca954b96f34bf5b8320f0c01a1bc2dcba6
Cyber-fungus <internationalspy17@gmail.com>
2026-08-22 13:16:06 +0530

    feat: add basic self-attention implementation

---
+++ src/self_attention.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+class SelfAttention(nn.Module):
+    def __init__(self,d_in,d_out,qkv_bias=False):
+        super().__init__()
+        self.W_q = nn.Linear(d_in,d_out,bias=qkv_bias)
+        self.W_k = nn.Linear(d_in,d_out,bias=qkv_bias)
+        self.W_v = nn.Linear(d_in,d_out,bias=qkv_bias)
+
+    def forward(self,x):
+        queries = self.W_q(x)
+        keys = self.W_k(x)
+        values = self.W_v(x)
+
+        attn_scores = queries @ keys.T
+
+        attn_weight = torch.softmax(
+            attn_scores/keys.shape[-1] ** 0.5,
+            dim=-1
+        )
+        context_vector = attn_weight @ values
+        return context_vector
